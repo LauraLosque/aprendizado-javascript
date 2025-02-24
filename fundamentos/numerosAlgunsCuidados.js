7c5c7dde061038addbe6ed016a788572d48514af
LauraLosque <143561690+LauraLosque@users.noreply.github.com>
2025-02-24 15:31:20 -0300

    fundamentos
    fundamentos

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0) //qualquer numero dividido por 0 é sempre infinito
+console.log("10" / 2)
+console.log('3' + 2)
+console.log('3' - 2)
+console.log("Show!" * 2)
+console.log(0.1 + 0.7) //gera uma imprecisao
+//console.log(10.toString())
+console.log((10.345).toFixed(2))
